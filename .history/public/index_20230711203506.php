9b5dc3809ee8cc2209a7f8f24454d29a53e1ee9d
F-ksk <kfutamata22@gmail.com>
2023-07-12 02:10:57 +0900

    Initial Commit

---
+++ .history/public/index_20230711203506.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="robots" content="noindex" />
+  <title>ひとこと掲示板</title>
+  <link rel="stylesheet" href="./assets/main.css" />
+</head>
+
+<body>
+  <div class="page-cover">
+    <p class="page-title">ひとこと掲示板</p>
+    <hr class="page-driver" />
+    <div class="form-cover">
+        <!-- 投稿内容入力フォーム -->
+      <form action="/" method="post">
+        <div class="form-input-title">投稿者ニックネーム</div>
+        <input type="text" name="author-name" maxlength="40" value="" class="input-autor-name" />
+        <div class="form-input-error"></div>
+        <div class="form-input-title">投稿内容<small>(必須)</small></div>
+        
+      </form>
+    </div>
+    <hr class="page-divider" />
+      <div class="message-list-cover">
+        <!-- この中に投稿された内容のリストを表示 -->
+      </div>
+    <hr class="page-divider" />
+  </div>
+</body>
+
+</html>
